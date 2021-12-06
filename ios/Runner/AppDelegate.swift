c42c9b4071804b3aba13ddc0ce401c32fdb8e29d
muller <matheus_santos_2000@outlook.com>
2021-12-06 15:23:49 -0300

    feat: add ios method channels

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,32 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let methodChannel = FlutterMethodChannel(
+        name: "app.muller/platform_channel",
+        binaryMessenger: controller.binaryMessenger
+    )
+      
+    methodChannel.setMethodCallHandler({(call: FlutterMethodCall, result: FlutterResult) -> Void in
+        switch call.method {
+        case "version":
+            let data: [String: Any] = call.arguments as! [String: Any]
+            let name: String = data["name"] as! String
+            let lastName: String = data["lastName"] as! String
+            let age: Int = data["age"] as! Int
+            
+            print("Nome: \(name)")
+            print("Sobrenome: \(lastName)")
+            print("Idade: \(age)")
+            
+            let version = UIDevice().systemVersion
+            result("IOS \(version)")
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    })
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
